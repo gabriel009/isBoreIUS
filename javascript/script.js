beb7c5383fc36fe978928bd32c8e9b3ccca8c6fe
Gabriel Nascimento <gabriel.nascimento3110@outlook.com>
2020-05-31 15:50:03 -0300

    exchange input text for range

--- javascript/script.js
+++ javascript/script.js
@@ -1,25 +1,48 @@
-function borderRadius(){
-  var boxMain = document.getElementById('main-box')
-  
-  var boxInputTopLeft = document.getElementById('top-left').value + 'px'
-  var boxInputBottomLeft = document.getElementById('bottom-left').value + 'px'
+let boxMain = document.getElementById('main-box')
+let codeBorder = document.getElementById('code-border')
 
-  var boxInputTopRight = document.getElementById('top-right').value + 'px'
-  var boxInputBottomRight = document.getElementById('bottom-right').value + 'px'
+let rangeTopLeft = document.getElementById('rangeTopLeft')
+let rangeTopRight = document.getElementById('rangeTopRight')
+let rangeBottomLeft = document.getElementById('rangeBottomLeft')
+let rangeBottomRight = document.getElementById('rangeBottomRight')
 
-  boxMain.style.borderTopLeftRadius=boxInputTopLeft;
-  boxMain.style.borderBottomLeftRadius=boxInputBottomLeft;
-  boxMain.style.borderTopRightRadius=boxInputTopRight;
-  boxMain.style.borderBottomRightRadius=boxInputBottomRight;
+function rangeValue(handleRange){
+  if (handleRange === 'topLeft'){
+    valueRange = rangeTopLeft.value + '%'
+    boxMain.style.borderTopLeftRadius=valueRange;
+    let valueTopLeft =  rangeTopLeft.value;
+    return valueTopLeft
+  } 
+  else if (handleRange === 'topRight'){
+    valueRange = rangeTopRight.value + '%'
+    boxMain.style.borderTopRightRadius=valueRange;
+    let valueTopRight =  rangeTopRight.value;
+    return valueTopRight
+  }
+  else if(handleRange === 'bottomLeft'){
+    valueRange = rangeBottomLeft.value + '%'
+    boxMain.style.borderBottomLeftRadius=valueRange;
+    let valueBottomLeft = rangeBottomLeft.value;
+    return valueBottomLeft
+  }
+  else if(handleRange === 'bottomRight'){
+    valueRange = rangeBottomRight.value + '%'
+    boxMain.style.borderBottomRightRadius=valueRange;
+    let valueBottomRight = rangeBottomRight.value;
+    return valueBottomRight
+  }  
+}
 
-  var codeCopy = document.getElementById('code-copy')
-
-  codeCopy.innerHTML = `border-radius: ${boxInputTopLeft} ${boxInputTopRight} ${boxInputBottomRight} ${boxInputBottomLeft};`
-
+function refreshValue(){
+  let valueTopLeft = rangeValue('topLeft')
+  let valueTopRight = rangeValue('topRight')
+  let valueBottomLeft = rangeValue('bottomLeft')
+  let valueBottomRight = rangeValue('bottomRight')
+  codeBorder.innerHTML = `border-radius: ${valueTopLeft}% ${valueTopRight}%  ${valueBottomRight}% ${valueBottomLeft}%;`
 }
 
 function copyClipboard(){
-  const copyCode = document.getElementById('code-copy').textContent;
+  const copyCode = document.getElementById('code-border').textContent;
 
   const textArea = document.createElement('textarea');
   textArea.textContent = copyCode
@@ -27,6 +50,4 @@ function copyClipboard(){
   document.body.append(textArea);
   textArea.select();
   document.execCommand("copy")
-
-
 }
